14f482c2d7359939f24bedc42b69542e81f1c7b0
krishgabani13 <142239928+krishgabani13@users.noreply.github.com>
2024-02-13 00:01:26 +0530

    Problem Solved

---
+++ NumberBridge.java
@@ -0,0 +1,33 @@
+package PatternPrinting_Composite;
+
+import java.util.Scanner;
+
+public class NumberBridge {
+    public static void main(String[] args) {
+
+        Scanner SC = new Scanner(System.in);
+        System.out.print("Enter the no :");
+        int n = SC.nextInt();
+
+        for(int i = 1; i <= 2 * n - 1; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        n--;
+
+        for (int i = 1; i <= n; i++){
+            int a = 1;
+            for (int j = 1; j <= n + 1 - i; j++){
+                System.out.print(a++ + " ");
+            }
+            for (int j = 1; j <=2 * i - 1; j++){
+                System.out.print(" " + " ");
+                a++;
+            }
+            for (int j = 1; j <= n + 1 - i; j++){
+                System.out.print(a++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
